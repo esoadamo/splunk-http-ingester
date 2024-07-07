43c4a5ffda9b9af6cb762f43e9856af5bec74e95
Adam Hlavacek <git@adamhlavacek.com>
2024-07-07 09:57:37 +0200

    fix: silent curl

--- client/shi.bash
+++ client/shi.bash
@@ -15,4 +15,5 @@ cat | curl -X 'POST' \
   "$SHI_HOST/ingest?api_key=$SHI_API_KEY&source_type=$SHI_SOURCE_TYPE&source=$SHI_SOURCE&channel=$SHI_CHANNEL" \
   -H 'accept: application/json' \
   -H 'Content-Type: text/plain' \
+  --silent \
   --data-binary @-
